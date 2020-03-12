93d144b36b61cb066148f8b68ae428431ccb8eea
Aayush Joglekar <aayushjog@gmail.com>
2020-03-12 16:26:28 +0530

    Added Blind 75 problems

---
+++ blind75/climbing_stairs.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+
+        if (n < 3)
+        {
+            return n;
+        }
+
+        vector<int> res(n, 0);
+
+        res[0] = 1;
+        res[1] = 2;
+
+        for (int i = 2; i < n; i++)
+        {
+            res[i] = res[i - 1] + res[i - 2];
+        }
+
+        return res[n - 1];
+    }
+};
