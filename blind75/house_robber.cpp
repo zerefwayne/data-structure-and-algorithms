28d4349781be411d88ca6e37cebcba4ac75d7889
Aayush Joglekar <aayushjog@gmail.com>
2020-03-15 05:58:21 +0530

    Added Blind 75 problems

---
+++ blind75/house_robber.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+
+        int n = nums.size();
+
+        vector<int> dp(n, 0);
+
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return nums[0];
+        }
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+
+        return dp[n - 1];
+    }
+};
