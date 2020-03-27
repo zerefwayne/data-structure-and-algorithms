6c5e3304d4b324d7425bc267253dfb7e2ebfe80a
Aayush Joglekar <aayushjog@gmail.com>
2020-03-27 17:57:52 +0530

    Modified solution B

--- codeforces/div_3_round_629/B.cpp
+++ codeforces/div_3_round_629/B.cpp
@@ -2,40 +2,37 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
     int t;
-    cin>>t;
+    cin >> t;
     int n;
     int k;
 
-    while(t--) {
+    while (t--)
+    {
 
-        cin>>n;
-        cin>>k;
+        cin >> n;
+        cin >> k;
 
-        int x=0;
-        int sum=0;
+        int x = 0;
+        int sum = 0;
 
-        while(sum < k) {
+        while (sum < k)
+        {
             x++;
-            sum = (x*(x+1)) / 2;
+            sum = (x * (x + 1)) / 2;
         }
 
         int one_loc = x - ((sum - k) + 1);
 
-        vector<char> resvec(n, 'a');
+        string resvec(n, 'a');
 
-        resvec[x] = 'b';
-        resvec[one_loc] = 'b';
+        resvec[n - x - 1] = 'b';
+        resvec[n - one_loc - 1] = 'b';
 
-        reverse(resvec.begin(), resvec.end());
-
-        for (char c: resvec) {
-            cout<<c;
-        }
-
-        cout<<'\n';
+        cout << resvec << '\n';
     }
 
     return 0;
